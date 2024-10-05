5886e249e682d4de5f576438e0ffdf591109c1ea
河西大地 <kasaidaichi@kasaidaichinoMacBook-Pro.local>
2024-10-05 23:02:52 +0900

    loginform complete

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once "db_userinfo_connect.php";
 require_once "functions.php";
 
@@ -18,7 +19,7 @@ if (!isset($_SESSION['csrf_token'])){
 }
 
 
-if ($_SESSION['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     checkToken();
 
     $datas = [
@@ -27,12 +28,42 @@ if ($_SESSION['REQUEST_METHOD'] == 'POST') {
         'confirm_password' => ''
     ];
 
-    foreach ( $datas as $key => $value) {
+    foreach ($datas as $key => $value) {
         if ($value = filter_input(INPUT_POST, $key, FILTER_DEFAULT)) {
+            $datas[$key] = $value;
+        }
+        echo $value;
     }
     
+
+    $errors = validation($datas, false);
+
+    if (empty($errors)) {
+        $sql = "SELECT id, name, password FROM user_info WHERE name= :name";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue('name',$datas['name'],PDO::PARAM_STR);
+        $stmt->execute();
+
+        if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            if (password_verify($datas['password'], $row['password'])) {
+                echo "ok";
+                session_regenerate_id();
+                $_SESSSION['login'] = true;
+                $_SESSION['id'] = $row['id'];
+                $_SESSION['name'] = $row['name'];
+                header("location:my_top.php");
+            }
+            else {
+                $login_err = "Invalid username or password";
+            }
+        }
+        else {
+            $login_err = "Invalid username or password";
+        }
+    }
+
 }
-###### if リクエストメソッドがpostの時、tokencheck, login
+
 
     
 
@@ -49,7 +80,30 @@ if ($_SESSION['REQUEST_METHOD'] == 'POST') {
     <title>Login</title>
 </head>
 <body>
-    <p>ログイン</p>
-    <p>テストテスト</p>
+    <h1>ログイン</h1>
+    <?php
+        if(!empty($login_err)){
+            echo  $login_err;
+        }
+    ?>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+            
+            <div>
+                <label>ユーザ名</label><br/>
+                <input type="text" name="name">
+            </div>
+            <div>
+                <label>パスワード</label><br/>
+                <input type="password" name="password">
+            </div>
+            
+            <div>
+                <br>
+                <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token']);?>">
+                <input type="submit" >
+            </div>
+
+            <p>登録をしていない方はこちら <a href="signup.php">サインアップ</a></p>
+        </form>
 </body>
 </html>
